9455751ef446765188556c0a76d33c8a5634e228
Midjan <midjan.begic@gmail.com>
2022-08-16 15:21:55 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -18,51 +18,56 @@ function App() {
   };
 
   return (
-    <div className="app">
-      <div className="search">
-        <input
-          value={location}
-          onChange={(event) => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder="Enter Location"
-          type="text"
-        />
-      </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed() - 273}°C</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+    <div>
+      <img src="logo.png" width={100} height={100} align="left" />
+      <div className="app">
+        <div className="search">
+          <input
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder="Enter Location"
+            type="text"
+          />
         </div>
-
-        {data.name !== undefined && (
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? (
-                <p className="bold">
-                  {data.main.feels_like.toFixed() - Math.floor(273.15)}°C
-                </p>
-              ) : null}
-              <p>Feels Like</p>
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
             </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed() - 273}°C</h1> : null}
             </div>
-            <div className="wind">
-              {data.wind ? (
-                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
-              ) : null}
-              <p>Wind Speed</p>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
             </div>
           </div>
-        )}
+
+          {data.name !== undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">
+                    {data.main.feels_like.toFixed() - Math.floor(273.15)}°C
+                  </p>
+                ) : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? (
+                  <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
